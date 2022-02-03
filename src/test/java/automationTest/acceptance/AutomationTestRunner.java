67fe92fcba3769ca4a625ae5cc7c7e7b2e1b01d5
Gabrielvduarte <gabrielvictorduarte58@gmail.com>
2022-02-03 14:18:16 -0300

    dentro da classe AutomationTestRunner removi a variavel tag

--- src/test/java/automationTest/acceptance/AutomationTestRunner.java
+++ src/test/java/automationTest/acceptance/AutomationTestRunner.java
@@ -6,8 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/ValidaTelaCarrinho.feature",
-        tags = "@UtilizaCupom"
+        features = "src/test/resources/features/ValidaTelaCarrinho.feature"
 )
 public class AutomationTestRunner {
 }
